1e33f5a935d0b01690a642bcf2dfb34e8be2aacf
Jonathan M. <neodev7440@gmail.com>
2026-02-11 10:40:26 +0100

    ajout de la model du client avec attributs

---
+++ src/model/Client.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Client {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDateTime createdAt;
+}
