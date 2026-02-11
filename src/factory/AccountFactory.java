01c3ef16b87cc4c92b3141b2a3fc638b2234ad2c
Jonathan M. <neodev7440@gmail.com>
2026-02-11 11:42:10 +0100

    ajout d'une méthode de creation de compte avec un switch case en fonction des types

--- src/factory/AccountFactory.java
+++ src/factory/AccountFactory.java
@@ -1,5 +1,21 @@
 package factory;
 
+import model.Account;
+import model.CurrentAccount;
+import model.SavingsAccount;
+
 public class AccountFactory {
 
+    /*--------------------méthodes----------------*/
+    public static Account createAccount(String type,Long id, String number, double balance, Long clientId) {
+        if (type == null || type.isEmpty()){
+            throw new IllegalArgumentException("Type is null or empty");
+        }
+
+        return switch (type.toUpperCase()) {
+            case "CURRENT" -> new CurrentAccount(id, number, balance, clientId, 0.0);
+            case "SAVINGS" -> new SavingsAccount(id, number, balance, clientId, 0.0);
+            default -> throw new IllegalArgumentException("Unknown account type " + type);
+        };
+    }
 }
