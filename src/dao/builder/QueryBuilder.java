8d538a07ffcf909d55b80cdda8062fe21916bbfb
Jonathan M. <neodev7440@gmail.com>
2026-02-11 13:12:42 +0100

    ajout des attribut + constructeur

--- src/dao/builder/QueryBuilder.java
+++ src/dao/builder/QueryBuilder.java
@@ -1,4 +1,18 @@
 package dao.builder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QueryBuilder {
+    private StringBuilder query;
+    private List<String> whereConditions;
+    private List<String> orderByColumns;
+
+    private QueryBuilder(){
+        this.query = new StringBuilder();
+        this.whereConditions = new ArrayList<>();
+        this.orderByColumns = new ArrayList<>();
+    }
+    
+
 }
