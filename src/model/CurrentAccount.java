6b8f0f221e68762f4ea655e11afc0dc66d585ee8
Jonathan M. <neodev7440@gmail.com>
2026-02-11 10:55:12 +0100

    ajout de constructeur + attribut overdraft + getter et setter

--- src/model/CurrentAccount.java
+++ src/model/CurrentAccount.java
@@ -1,11 +1,29 @@
 package model;
 
 public class CurrentAccount  extends Account {
+    private double overdraftLimit;
 
+    /* --------------Constructeurs---------------*/
     public CurrentAccount(){
         super();
     }
 
+    public CurrentAccount(Long id, String number, double balance, Long clientId, double overdraftLimit) {
+        super(id, number, balance, clientId);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    /* -------------getters et setters-------------------*/
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+
+    public void setOverdraftLimit(double overdraftLimit) {
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    /*----------------méthodes----------------------*/
     @Override
     public String getType() {
         return "CURRENT";
