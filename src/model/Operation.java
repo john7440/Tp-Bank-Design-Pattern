b96c30ad177b79f435a8fc7c0410fb7fbe3dc2e7
Jonathan M. <neodev7440@gmail.com>
2026-02-11 11:13:41 +0100

    ajout constructeur + getter et setters

--- src/model/Operation.java
+++ src/model/Operation.java
@@ -8,4 +8,57 @@ public class Operation {
     private Double amount;
     private LocalDateTime operationDate;
     private Long accountId;
+
+    /*-------------Constructeurs----------------*/
+    public Operation() {
+    }
+
+    public Operation(Long id, String type, Double amount, LocalDateTime operationDate, Long accountId) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+        this.operationDate = operationDate;
+        this.accountId = accountId;
+    }
+
+    /*-----------------getter et setter --------------------------*/
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getOperationDate() {
+        return operationDate;
+    }
+
+    public void setOperationDate(LocalDateTime operationDate) {
+        this.operationDate = operationDate;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
 }
