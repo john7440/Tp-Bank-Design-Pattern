5460227381a93fe6ffb87536c3d4e64dab0ef913
Jonathan M. <neodev7440@gmail.com>
2026-02-11 11:03:24 +0100

    ajout des constructeurs + getter/setter

--- src/model/SavingsAccount.java
+++ src/model/SavingsAccount.java
@@ -1,9 +1,24 @@
 package model;
 
 public class SavingsAccount extends Account {
+    private double interestRate;
 
+    /*----------------Constructeurs-------------------------*/
     public  SavingsAccount() {
         super();
+    }
+    public SavingsAccount(Long id, String number,double balance, Long clientID, double interestRate) {
+        super(id, number, balance, clientID);
+        this.interestRate = interestRate;
+    }
+
+    /*-------------------getter/setter---------------*/
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
     }
 
     @Override
