3c6929755f7bd2af820ed34d88168c71d18c80c4
Jonathan M. <neodev7440@gmail.com>
2026-02-11 13:05:19 +0100

    ajout de  la connexion au singleton

--- src/dao/AccountDao.java
+++ src/dao/AccountDao.java
@@ -1,4 +1,14 @@
 package dao;
 
+import database.DatabaseConnection;
+import java.sql.Connection;
+
 public class AccountDao {
+    private Connection connection;
+
+    public AccountDao() {
+        this.connection = DatabaseConnection.getInstance().getConnection();
+    }
+
+
 }
