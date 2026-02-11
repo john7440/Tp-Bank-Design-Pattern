c9c9c11f628bb005c77ff461bca1b62972920e74
Jonathan M. <neodev7440@gmail.com>
2026-02-11 10:29:47 +0100

    ajout de la classe model account avec ses attributs

---
+++ src/model/Account.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Account {
+    private Long id;
+    private String number;
+    private Double balance;
+    private Long clientId;
+    private LocalDateTime createdAt;
+}
