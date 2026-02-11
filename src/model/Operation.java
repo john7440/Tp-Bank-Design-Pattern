66677211ff475315fbbcf4ab23c472b5ce1a4213
Jonathan M. <neodev7440@gmail.com>
2026-02-11 11:11:20 +0100

    ajout de la class model pour les operations

---
+++ src/model/Operation.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Operation {
+    private Long id;
+    private String type;
+    private Double amount;
+    private LocalDateTime operationDate;
+    private Long accountId;
+}
