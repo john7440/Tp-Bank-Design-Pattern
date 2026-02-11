49137452a795b59183828048e1fd1909f9314f96
Jonathan M. <neodev7440@gmail.com>
2026-02-11 15:03:33 +0100

    ajout de la methode pour recuperer toutes les operations

--- src/dao/OperationDao.java
+++ src/dao/OperationDao.java
@@ -1,15 +1,42 @@
 package dao;
 
+import dao.builder.QueryBuilder;
+import dao.mapping.MapResultSetHelper;
 import database.DatabaseConnection;
+import model.Operation;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OperationDao {
     private Connection connection;
+    private MapResultSetHelper helper =  new MapResultSetHelper();
 
     public OperationDao() {
         this.connection = DatabaseConnection.getInstance().getConnection();
     }
 
+    public List<Operation> findAll() {
+        List<Operation> operations = new ArrayList<>();
+        String sql= QueryBuilder
+                .select("*")
+                .from("operations")
+                .orderBy("operation_date DESC")
+                .build();
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)){
+
+            while (rs.next()){
+                operations.add(helper.mapResultSetToOperation(rs));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error fetching all operations",e);
+        }
+        return operations;
+    }
 
 }
