addd8dc54857dd512c78ef4c78916afedd4c6446
Jonathan M. <neodev7440@gmail.com>
2026-02-11 10:42:24 +0100

    ajout des constructeurs + getter et setters

--- src/model/Client.java
+++ src/model/Client.java
@@ -8,4 +8,56 @@ public class Client {
     private String email;
     private String phone;
     private LocalDateTime createdAt;
+
+    /*-------------Constructeurs- -------------------*/
+    public Client() {
+    }
+
+    public Client(Long id, String name, String email, String phone, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    /* -------getter et setter-------------*/
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
