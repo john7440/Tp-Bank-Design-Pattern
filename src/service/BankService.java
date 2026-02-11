a7bf73e836b6a9ce9fa0df787ad43a183a024248
Jonathan M. <neodev7440@gmail.com>
2026-02-11 15:42:28 +0100

    ajout des méthodes de gestion client

--- src/service/BankService.java
+++ src/service/BankService.java
@@ -3,6 +3,12 @@ package service;
 import dao.AccountDao;
 import dao.ClientDao;
 import dao.OperationDao;
+import exception.AccountNotFoundException;
+import model.Account;
+import model.Client;
+
+
+import java.util.List;
 
 public class BankService {
     private ClientDao clientDao;
@@ -14,4 +20,24 @@ public class BankService {
         this.accountDao = new AccountDao();
         this.operationDao = new OperationDao();
     }
+
+    /*----------------------gestion des clients----------------*/
+    public List<Client> getAllClients(){
+        return clientDao.getAllClients();
+    }
+
+    public Client findClientById(long id){
+        Client client = clientDao.findById(id);
+        if (client ==null){
+            throw new AccountNotFoundException("Client not found: "+ id);
+        }
+        return client;
+    }
+
+
+   /*------------------gestion comptes--------------------*/
+    public List<Account> getAllAccounts(){
+        return accountDao.findAll();
+    }
+    
 }
