31835709695c2f4dcce7182f865d08c280e7c571
Jonathan M. <neodev7440@gmail.com>
2026-02-12 09:25:06 +0100

    ajout d'une methode pour gérer les int avec un try catch

--- src/view/InputHelper.java
+++ src/view/InputHelper.java
@@ -8,4 +8,16 @@ public class InputHelper {
         System.out.println(prompt);
         return scanner.nextLine().trim();
     }
+
+    public static int readInt(Scanner scanner,String prompt) {
+        while(true){
+            try{
+                System.out.println(prompt);
+                String input = scanner.nextLine().trim();
+                return  Integer.parseInt(input);
+            } catch(NumberFormatException e){
+                System.out.println("Enter a valid number");
+            }
+        }
+    }
 }
