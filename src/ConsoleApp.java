943b1c8785e0a0c3e58244a20c0a2b283b3c7d53
Jonathan M. <neodev7440@gmail.com>
2026-02-12 10:27:37 +0100

    ajout d'une banniere de bienvenue

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -1,12 +1,21 @@
 import service.BankService;
+import view.MenuView;
 
 import java.util.Scanner;
 
 public class ConsoleApp {
     private static BankService bankService = new BankService();
     private static Scanner scanner = new Scanner(System.in);
+    private static MenuView menuView = new MenuView(bankService, scanner);
 
     public static void main(String[] args) {
+        displayWelcomeBanner();
 
+    }
+
+    private static void displayWelcomeBanner() {
+        System.out.println("\n" + "=".repeat(60));
+        System.out.println("        WELCOME");
+        System.out.println("=".repeat(60));
     }
 }
