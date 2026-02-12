d3b39ddfdb7f3c37c7bb0e1fcb8ad80f87e88c61
Jonathan M. <neodev7440@gmail.com>
2026-02-12 09:22:52 +0100

    ajout de la methode pour gerer les String

--- src/view/InputHelper.java
+++ src/view/InputHelper.java
@@ -1,4 +1,11 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputHelper {
+
+    public static String readString(Scanner scanner,String prompt) {
+        System.out.println(prompt);
+        return scanner.nextLine().trim();
+    }
 }
