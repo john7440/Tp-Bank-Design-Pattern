c344c73e23f1db672aa670865dedd876112b4b35
Jonathan M. <neodev7440@gmail.com>
2026-02-12 09:01:42 +0100

    ajout du point d'entré de l'application

---
+++ src/ConsoleApp.java
@@ -0,0 +1,12 @@
+import service.BankService;
+
+import java.util.Scanner;
+
+public class ConsoleApp {
+    private static BankService bankService = new BankService();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
